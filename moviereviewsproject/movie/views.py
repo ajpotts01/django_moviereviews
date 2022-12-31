71c7149cc63ad2cc76e9060c60a7a7bf10ea6e9e
Alexander Potts <alex.potts@edg.com.au>
2022-12-31 19:25:01 +1100

    End of Chapter 4: Templates

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
-def home(request):
-    return HttpResponse('<h1>Welcome to the home page</h1>')
+def home(request: HttpRequest):
+    SEARCH_COMPONENT: str = 'search-movie'
 
-def about(request):
+    search_term: str = request.GET.get(SEARCH_COMPONENT)
+    return render(request, 'home.html', {
+        'search_term': search_term
+    })
+
+def about(request: HttpRequest):
     return HttpResponse('<h1>Welcome to the About page</h1>')
+
+def signup(request: HttpRequest):
+    EMAIL_COMPONENT = 'email'
+
+    email = request.GET.get(EMAIL_COMPONENT)
+    return render(request, 'signup.html', {
+        'email': email
+    })
