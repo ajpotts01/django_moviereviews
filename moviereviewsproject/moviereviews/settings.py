8bf65139315d565f079a70ee12aac0785681dce7
Alexander Potts <alex.potts@edg.com.au>
2023-01-01 21:01:36 +1100

    End of Chapter 7: Extending Templates

--- moviereviewsproject/moviereviews/settings.py
+++ moviereviewsproject/moviereviews/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'moviereviews/static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
